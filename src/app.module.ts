405ce0531d1cacbc51cd2fb6cdf03c94b08ac0c8
Jason_song97 <jaegeunsong97@gmail.com>
2023-10-31 10:50:10 +0900

    TypeORM과 Postgresql 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({ // forRoot(): TypeORM-NestJS 연결고리
+      type: 'postgres', // 데이터베이스 타입
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [], // 데이터와 연동될 모델
+      synchronize: true, // TypeORM-DB 싱크 맞추기 -> 개발환경 true | production모드 false
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
