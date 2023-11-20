d08e59bd993833a5a92da169bda2318c15997eb1
Jason_song97 <jaegeunsong97@gmail.com>
2023-11-20 10:54:04 +0900

    abstract BaseModel 사용해서 OOP적용

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,11 +1,9 @@
+import { BaseModel } from "src/common/entity/base.entity";
 import { UsersModel } from "src/users/entities/users.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
 
 @Entity()
-export class PostsModel { // 외부에서 사용할 수 있어야하니까 export
-
-     @PrimaryGeneratedColumn()
-     id: number;
+export class PostsModel extends BaseModel{ // 외부에서 사용할 수 있어야하니까 export
 
      // 1) UsersModel 연동, FK를 이용해서
      // 2) Null이 될 수 없다
