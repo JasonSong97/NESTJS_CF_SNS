ea8a2424cfffb70b379630614c47c135df134621
Jason_song97 <jaegeunsong97@gmail.com>
2023-11-25 12:56:28 +0900

    Class Validator 에러 메세지 변경

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,11 +1,16 @@
 import { IsString } from "class-validator";
 
 // yarn add class-validator(검증) class-transformer(변환)
+// github.com/typestack/class-validator -> validator-decorators
 export class CreatePostDto {
 
-     @IsString()
+     @IsString({
+          message: 'title은 string 타입을 입력 해줘야합니다.'
+     })
      title: string;
 
-     @IsString() 
+     @IsString({
+          message: 'content는 string 타입을 입력 해줘야합니다.'
+     }) 
      content: string;
 }
