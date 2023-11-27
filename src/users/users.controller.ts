c0b088429be503200fa77804bd9d45c78d776365
Jason_song97 <jaegeunsong97@gmail.com>
2023-11-27 17:26:17 +0900

    ClassSerializer AppModule에 적용

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,7 +7,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  @UseInterceptors(ClassSerializerInterceptor) // 하는 역할: user Entity에서 @Exclude() 확인
+  //@UseInterceptors(ClassSerializerInterceptor) // 하는 역할: user Entity에서 @Exclude() 확인
   /**
    * serialization -> 직렬화 -> 현재 시스템에서 사용되는 (Nest.js) 데이터의 구조를 다른 사스템에서도 쉽게
    *                           사용할 수 있는 포맷으로 변환
