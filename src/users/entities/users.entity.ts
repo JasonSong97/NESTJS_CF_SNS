188b1851850ef9ed4a8eb2c00d76ca3e0f9ffa26
Jason_song97 <jaegeunsong97@gmail.com>
2023-11-27 17:33:00 +0900

    Expose Annotation 학습

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -6,7 +6,7 @@ import { IsEmail, IsString, Length } from "class-validator";
 import { lengthValidationMessage } from "src/common/validation-message/length-validation.message";
 import { stringValidationMessage } from "src/common/validation-message/string-validation.message";
 import { emailValidationMessage } from "src/common/validation-message/email-validation.message";
-import { Exclude } from "class-transformer";
+import { Exclude, Expose } from "class-transformer";
 
 @Entity()
 export class UsersModel extends BaseModel{
@@ -17,35 +17,21 @@ export class UsersModel extends BaseModel{
           // 2)
           unique: true,
      })
-     @IsString({
-          message: stringValidationMessage,
-     })
-     @Length(1, 20,{
-          message: lengthValidationMessage,
-     })
+     @IsString({message: stringValidationMessage,})
+     @Length(1, 20,{message: lengthValidationMessage,})
      // 1) 길이가 20넘지 않기
      // 2) 유니크
      nickname: string;
 
-     @Column({
-          unique: true
-     })
-     @IsString({
-          message: stringValidationMessage,
-     })
-     @IsEmail({}, {
-          message: emailValidationMessage,
-     })
+     @Column({unique: true})
+     @IsString({message: stringValidationMessage,})
+     @IsEmail({}, {message: emailValidationMessage,})
      // 1) 유니크
      email: string;
 
      @Column()
-     @IsString({
-          message: stringValidationMessage,
-     })
-     @Length(3, 8,{
-          message: lengthValidationMessage,
-     })
+     @IsString({message: stringValidationMessage,})
+     @Length(3, 8,{message: lengthValidationMessage,})
      /**
       * Request: toClassOnly -> class instance로 변환될 때만
       * front -> back: plain object (JSON) -> class instance (DTO)
