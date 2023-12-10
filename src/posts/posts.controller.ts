ed6bc362bc6d94f1670f9b43db72e789e790e7d1
Jason_song97 <jaegeunsong97@gmail.com>
2023-12-10 20:56:04 +0900

    랜덤데이터 생성하는 로직 만들기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -5,6 +5,7 @@ import { User } from 'src/users/decorator/user.decorator';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PaginatePostDto } from './dto/paginate-post.dto';
+import { UsersModel } from 'src/users/entities/users.entity';
 
 @Controller('posts')
 export class PostsController {
@@ -19,6 +20,14 @@ export class PostsController {
   ) {
     return this.postsService.paginatePosts(query);
     //return this.postsService.getAllPosts();
+  }
+
+  // POST /posts/random
+  @Post('random')
+  @UseGuards(AccessTokenGuard)
+  async postPostsRandom(@User() user: UsersModel) {
+    await this.postsService.generatePosts(user.id);
+    return true;
   }
 
   // 2) GET /posts/:id
